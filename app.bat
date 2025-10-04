712418f11e3bcf0d314d2ab4fdbb5d04981a5194
Belgarion <dev.rorybolan@gmail.com>
2025-10-04 11:25:44 +0200

    add bat script

---
+++ app.bat
@@ -0,0 +1,37 @@
+@echo off
+set REPO_NAME=app-discord
+
+if exist "%REPO_NAME%" (
+    echo Repository already exists. Updating...
+    cd %REPO_NAME%
+    git pull
+) else (
+    echo Cloning repository...
+    git clone https://github.com/dev-rorybolan/app-discord.git
+    if errorlevel 1 (
+        echo Failed to clone repository!
+        pause
+        exit /b 1
+    )
+    cd %REPO_NAME%
+)
+
+if errorlevel 1 (
+    echo Failed to change directory!
+    pause
+    exit /b 1
+)
+
+echo Installing dependencies...
+pip install -r requirements.txt
+
+if errorlevel 1 (
+    echo Failed to install dependencies!
+    pause
+    exit /b 1
+)
+
+echo Running application...
+python src/main.py
+
+pause
